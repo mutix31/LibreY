af35696f5217c4f4a044709fe1912e29ff8c5908
Thomas DC <ThomasDC84@users.noreply.github.com>
2024-06-09 12:34:01 +0200

    Italian translation
    Addition of Italian translation for LibreY

---
+++ locale/it.php
@@ -0,0 +1,56 @@
+<?php
+
+return array(
+    "page_title" => "LibreY Ricerca",
+    "search_button" => "Cerca con LibreY",
+    "torrent_search_button" => "Cerca torrent con LibreY",
+
+    "source_code_link" => "Sorgente",
+    "instances_link" => "Instanze",
+    "settings_link" => "Impostazioni",
+    "api_link" => "API",
+    "donate_link" => "Dona ❤️",
+
+    "latest_commit" => "Ultimo aggiornamento: %s",
+
+    "category_general" => "Generale",
+    "category_images" => "Immagini",
+    "category_videos" => "Video",
+    "category_torrents" => "Torrent",
+    "category_tor" => "Tor",
+    "category_maps" => "Mappe",
+
+    "feature_disabled" => "L'istanza ha disabilitato questa funzione :C",
+
+    "settings_title" => "Impostazioni",
+    "settings_theme" => "Tema",
+    "settings_special_disabled" => "Disabilitare le ricerche speciali (ad es.: conversione di valuta)",
+
+    "settings_frontends" => "Intermediari che rispettano la privacy",
+    "settings_frontends_description" => "Per esempio, se volete vedere YouTube senza essere spiati, cliccate su “Invidious”, trovate l'istanza più adatta a voi e incollatela (formato corretto: https://example.com).",
+    "settings_frontends_disable" => "Disabilita gli intermediari",
+
+    "settings_search_settings" => "Impostazioni di ricerca",
+    "settings_language" => "Lingua",
+    "settings_preferred_engine" => "Motore preferito",
+
+    "settings_number_of_results" => "Numero di risultati per pagina",
+
+    "settings_safe_search" => "Ricerca sicura",
+    "settings_save" => "Salva",
+    "settings_reset" => "Ripristina",
+
+
+    "failure_fallback" => "Nessun risultato trovato. Impossibile passare ad altre istanze",
+    "failure_empty" => "Nessun risultato trovato. Provare con altri termini di ricerca!",
+    "result_no_description" => "Non è stata fornita alcuna descrizione per questo sito.",
+
+    "instances_librex" => "Le seguenti istanze stanno eseguendo la vecchia %s",
+
+    "donate_original_developer" => "Fate una donazione allo sviluppatore originale di %s, un progetto che LibreY cerca di migliorare.",
+    "donate_fork" => "Donare alla persona che ha fatto il fork di %s in LibreY",
+
+    "api_unavailable" => "Questa API di LibreY non è al momento disponibile."
+);
+
+?>
