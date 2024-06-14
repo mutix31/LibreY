1c95b6e7a379dd911396cc7ec45467072adcdb92
Ahwx <ahwx@ahwx.org>
2024-06-14 13:14:44 +0200

    feat(locale/en.php): adds english translation instead of dutch one :p

--- locale/en.php
+++ locale/en.php
@@ -24,7 +24,7 @@ return array(
 
     "settings_title" => "Settings",
     "settings_theme" => "Theme",
-    "settings_special_warning" => "Als u dit selectievakje inschakelt, blijft de instelling actief totdat u op de Reset knop klikt.",
+    "settings_special_warning" => "This checkbox will persist until the 'Reset' button is clicked or your cookies are cleared.",
     "settings_special_disabled" => "Disable special queries (e.g.: currency conversion)",
 
     "settings_frontends" => "Privacy friendly frontends",
