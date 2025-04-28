5cffa2e4134ace1fad98976d4d0addd231c5be49
mutix31 <102666146+mutix31@users.noreply.github.com>
2025-04-28 05:47:03 +0300

    Create tr.php

---
+++ locale/tr.php
@@ -0,0 +1,57 @@
+<?php
+
+return array(
+    "page_title" => "LibreY Arama",
+    "search_button" => "LibreY ile arama yapın.",
+    "torrent_search_button" => "LibreY ile torrentleri arayın.",
+
+    "source_code_link" => "Kaynak",
+    "instances_link" => "Örnekler",
+    "settings_link" => "Ayarlar",
+    "api_link" => "API",
+    "donate_link" => "Bağış ❤️",
+
+    "latest_commit" => "Son gönderim: %s",
+
+    "category_general" => "Genel",
+    "category_images" => "Görseller",
+    "category_videos" => "Videolar",
+    "category_torrents" => "Torrentler",
+    "category_tor" => "Tor",
+    "category_maps" => "Haritalar",
+
+    "feature_disabled" => "Alan sahibi bu özelliği devre dışı bıraktı :C",
+
+    "settings_title" => "Ayarlar",
+    "settings_theme" => "Temalar",
+    "settings_special_warning" => "Bu kontrol kutusu, 'Düzelt' düğmesine tıklanana veya çerezleriniz silene kadar kalır.",
+    "settings_special_disabled" => "Özel sorguları devre dışı bırakın (örneğin: para birimi dönüştürme)",
+
+    "settings_frontends" => "Gizlilik dostu ön uçlar",
+    "settings_frontends_description" => "Örneğin, YouTube'u gözetlenmeden izlemek istiyorsanız, \"Invidious\" seçeneğine tıklayın, sizin için en uygun örneği bulun ve yapıştırın (doğru format: https://example.com)",
+    "settings_frontends_disable" => "Ön uçları devre dışı bırakın",
+
+    "settings_search_settings" => "Arama ayarları",
+    "settings_language" => "Dil",
+    "settings_preferred_engine" => "Tercih Edilen Motor",
+
+    "settings_number_of_results" => "Sayfa başına sonuç sayısı",
+
+    "settings_safe_search" => "Güvenli arama",
+    "settings_save" => "Kaydet",
+    "settings_reset" => "Sıfırla",
+
+
+    "failure_fallback" => "Sonuç bulunamadı. Diğer örneklere geri dönüş yapılamıyor.",
+    "failure_empty" => "Sonuç bulunamadı. Lütfen farklı anahtar kelimeler deneyin!",
+    "result_no_description" => "Bu site için herhangi bir açıklama sağlanmadı.",
+
+    "instances_librex" => "Aşağıdaki örnekler eski sürümü çalıştırıyor %s",
+
+    "donate_original_developer" => "Orijinal geliştiriciye bağış yapın %s, LibreY'in geliştirmeye çalıştığı bir proje.",
+    "donate_fork" => "Çatallanan kişiye bağış yapın %s LibreY'e",
+
+    "api_unavailable" => "Bu LibreY API'si şu anda kullanılamıyor."
+);
+
+?>
